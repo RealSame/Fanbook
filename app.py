e979f09941c1b143becfe909d455ac943ec9dbde
Syahrial <syahrialbunder@gmail.com>
2023-05-17 09:10:07 +0700

    .

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route("/homework HTTPS", methods=["POST"])
+@app.route("/homework", methods=["POST"])
 def homework_post():
     # sample_receive = request.form['sample_give']
     name_receive = request.form['name_give']
